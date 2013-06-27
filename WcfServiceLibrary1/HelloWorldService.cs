d341c271acd288ea2611898db3536b32b4d51f87
heather_j_milligan@hotmail.com <heather_j_milligan@hotmail.com>
2013-06-27 21:26:34 +0100

    created

---
+++ WcfServiceLibrary1/HelloWorldService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldService
+{
+    [DataContract]
+    public class Name
+    {
+        [DataMember]
+        public string first;
+        [DataMember]
+        public string last;
+    }
+
+    [ServiceContract]
+    public interface IHelloWorld
+    {
+        [OperationContract]
+        string SayHello(Name person);
+    }
+
+    public class HelloWorldService: IHelloWorld
+    {
+
+        public string SayHello(Name person)
+        {
+            return string.Format("Hello {0} {1}", person.first, person.last);
+        }
+    }
+}
